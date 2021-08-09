107ca418c0221b5cda9fd386f85813a40bfb6f21
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-08-09 20:44:04 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,15 +1,20 @@
+
+// bgColor
 var bg_clr = Math.floor(Math.random() * 16777215).toString(16);
-clr_link = ("000000" + bg_clr).slice(-6);
 bg_clr = "#" + ("000000" + bg_clr).slice(-6);
 document.bgColor = bg_clr;
-setTimeout(() => { console.log(bg_clr) }, 40);
+// more info button
+clr_link = ("000000" + bg_clr).slice(-6);
+link = ("https://www.color-hex.com/color/" + clr_link)
+//
+document.fgColor = bg_clr;
 document.title = bg_clr + "- random color";
+document.querySelector('meta[name="theme-color"]').setAttribute("content", bg_clr);
 document.getElementById('txt').innerHTML = bg_clr;
 document.getElementById('div1').innerHTML = bg_clr;
-link = ("https://www.color-hex.com/color/" + clr_link)
+//
+setTimeout(() => { console.log(bg_clr) }, 40);
 link1 = ("https://www.github.com/maciekkoks")
-document.fgColor = bg_clr;
-document.querySelector('meta[name="theme-color"]').setAttribute("content", bg_clr);
 // copy
 function CopyToClipboard(containerid) {
     if (document.selection) {
