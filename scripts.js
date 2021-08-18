974abaae71d0d305f6d932d5f3a8141c2e83d7e9
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-08-18 21:37:57 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,16 +1,17 @@
-// bgColor
+function main() {
 var bg_clr = Math.floor(Math.random() * 16777215).toString(16);
 bg_clr = "#" + ("000000" + bg_clr).slice(-6);
-document.bgColor = bg_clr;
 clr_link = ("000000" + bg_clr).slice(-6);
+document.bgColor = bg_clr;
+setTimeout(() => { console.log(bg_clr) }, 40);
+document.title = bg_clr + "- random color";
+document.getElementById('txt').innerHTML = bg_clr;
+document.getElementById('div1').innerHTML = bg_clr;
 link = ("https://www.color-hex.com/color/" + clr_link)
 link1 = ("https://www.github.com/maciekkoks")
 document.fgColor = bg_clr;
-document.title = bg_clr + "- random color";
 document.querySelector('meta[name="theme-color"]').setAttribute("content", bg_clr);
-document.getElementById('txt').innerHTML = bg_clr;
-document.getElementById('div1').innerHTML = bg_clr;
-setTimeout(() => { console.log(bg_clr) }, 40);
+document.querySelector('input[type="color"]').setAttribute("value", bg_clr);
 //rgb
 String.prototype.convertToRGB = function(){
   var aRgbHex = this.match(/.{1,2}/g);
@@ -22,7 +23,6 @@ String.prototype.convertToRGB = function(){
   return aRgb;
 }
 document.getElementById('divrgb').innerHTML = ('RGB ') + clr_link.convertToRGB();
-console.log(clr_link.convertToRGB());
 // copy
 function CopyToClipboard(containerid) {
     if (document.selection) {
@@ -42,3 +42,5 @@ function CopyToClipboard(containerid) {
     var tooltip = document.getElementById("myTooltip");
     tooltip.innerHTML = "Copied: " + bg_clr;
   }
+}
+main();
