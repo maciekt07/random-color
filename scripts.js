4e04653e1cb65bc2aa86e0d6cf9c123d226be757
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-09-19 14:28:06 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -12,7 +12,7 @@ function main() {
   
   // title
   window.onblur = function() { document.title = bg_clr + " - random color" };
-  window.onfocus = function() { document.title = ("🟣Random Color🟣") };
+  window.onfocus = function() { document.title = ("🟣 Random Color 🟣") };
   
   link = ("https://www.color-hex.com/color/" + clr_link);
   link1 = ("https://www.github.com/maciekkoks");
