adb0388bc347021be332a4c3a28ae8bb7b68e56c
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-09-19 14:17:01 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -4,16 +4,18 @@ function main() {
   clr_link = ("000000" + bg_clr).slice(-6);
   
   document.bgColor = bg_clr;
-  document.title = bg_clr + " - random color";
   document.getElementById('txt').innerHTML = bg_clr;
   document.getElementById('div1').innerHTML = bg_clr;
-  
-  link = ("https://www.color-hex.com/color/" + clr_link)
-  link1 = ("https://www.github.com/maciekkoks")
   document.fgColor = bg_clr;
   document.querySelector('meta[name="theme-color"]').setAttribute("content", bg_clr);
   document.querySelector('input[type="color"]').setAttribute("value", bg_clr);
   
+  // title
+  window.onblur = function() { document.title = bg_clr + " - random color" };
+  window.onfocus = function() { document.title = ("Random Color") };
+  
+  link = ("https://www.color-hex.com/color/" + clr_link);
+  link1 = ("https://www.github.com/maciekkoks");
   
   //rgb
   String.prototype.convertToRGB = function(){
