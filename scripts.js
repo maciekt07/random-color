a8fa4f95fb773d96799ac8b3b0a18afcae09ebaa
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-09-20 15:21:44 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -2,26 +2,18 @@ function main() {
   var bg_clr = Math.floor(Math.random() * 16777215).toString(16);
   bg_clr = "#" + ("000000" + bg_clr).slice(-6);
   clr_link = ("000000" + bg_clr).slice(-6);
-  
   document.bgColor = bg_clr;
-  document.fgColor = bg_clr;
-  document.getElementById('txt').innerHTML = bg_clr;
-  document.getElementById('div1').innerHTML = bg_clr;
-  document.getElementById('divrgb').innerHTML = ('RGB ') + clr_link.convertToRGB();
-  document.getElementById('historylist').innerHTML += "<li>" + bg_clr + (" | ") + (" RGB ") + clr_link.convertToRGB() + "<hr><br></li>";
-  document.getElementById("alert").style.display = "none";
-  document.getElementById('alertspan').innerHTML = "Copied to clipboard: " + bg_clr;
-  document.querySelector('meta[name="theme-color"]').setAttribute("content", bg_clr);
-  document.querySelector('input[type="color"]').setAttribute("value", bg_clr);
   
- 
-  // title
-  window.onblur = function() { document.title = bg_clr + " - Random Color" };
+  window.onblur = function() { document.title = bg_clr + " - random color" };
   window.onfocus = function() { document.title = ("Random Color") };
   
-  link = ("https://www.color-hex.com/color/" + clr_link);
-  link1 = ("https://www.github.com/maciekkoks");
-  
+  document.getElementById('txt').innerHTML = bg_clr;
+  document.getElementById('div1').innerHTML = bg_clr;
+  link = ("https://www.color-hex.com/color/" + clr_link)
+  link1 = ("https://www.github.com/maciekkoks")
+  document.fgColor = bg_clr;
+  document.querySelector('meta[name="theme-color"]').setAttribute("content", bg_clr);
+  document.querySelector('input[type="color"]').setAttribute("value", bg_clr);
   //rgb
   String.prototype.convertToRGB = function(){
     var aRgbHex = this.match(/.{1,2}/g);
@@ -31,16 +23,18 @@ function main() {
         parseInt(aRgbHex[2], 16)
     ];
     return aRgb;
- }
-  
+  }
   console.log(bg_clr);
   console.log(document.getElementById('divrgb').innerHTML = ('RGB ') + clr_link.convertToRGB());
+  document.getElementById('divrgb').innerHTML = ('RGB ') + clr_link.convertToRGB();
+  document.getElementById('historylist').innerHTML += "<li>" + bg_clr + (" | ") + ("RGB ") + clr_link.convertToRGB() + "<hr><br></li>";
   console.log('%c=-=-=-=-=-=-= ', 'color:#bf66ff; font-size: 18px;');
+  
+  document.getElementById("alert").style.display = "none";
+  document.getElementById('alertspan').innerHTML = "Copied to clipboard: " + bg_clr;
   }
-  
-main();
-  
-//show history button
+  main();
+  //show history button
   document.getElementById("history").style.display = "none";
   function showh() {
     var x = document.getElementById("history");
@@ -50,18 +44,15 @@ main();
       x.style.display = "none";
     }
   }
-  
-const btn = document.getElementById("hbutton");
+  const btn = document.getElementById("hbutton");
   btn.addEventListener("click", ()=>{
       if(btn.innerText === "show history"){
           btn.innerText = "hide history";
       }else{
           btn.innerText= "show history";
       }
- });
-  
-
-document.addEventListener('keyup', function(event) {
+  });
+  document.addEventListener('keyup', function(event) {
     if(event.keyCode == 82) { // r button changes color
         main()
     }
@@ -69,9 +60,8 @@ document.addEventListener('keyup', function(event) {
 function showa() {
     document.getElementById("alert").style.display = "block";
     setTimeout(function(){ document.getElementById("alert").style.display = "none"; }, 4500);
+    
 }
-
-
 // copy
 function copyToClipboard(element) {
   var $temp = $("<input>");
