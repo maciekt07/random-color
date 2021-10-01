1f7a8c596f628c4ccb1ad4d2caa3a4f6d5684afe
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2021-10-01 19:35:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -80,3 +80,30 @@ function copyToClipboard(element) {
     document.execCommand("copy");
     $temp.remove();
 }
+
+
+if (document.location.search.match(/type=embed/gi)) {
+    window.parent.postMessage("resize", "*");
+  }
+  // local storage
+let darkMode = localStorage.getItem('darkMode');
+const darkModeToggle = document.querySelector('#dark-mode-toggle');
+const enableDarkMode = () => {
+  document.body.classList.add('darkmode');
+  localStorage.setItem('darkMode', 'enabled');
+};
+const disableDarkMode = () => {
+  document.body.classList.remove('darkmode'); 
+  localStorage.setItem('darkMode', null);
+};
+if (darkMode === 'enabled') {
+  enableDarkMode();
+}
+darkModeToggle.addEventListener('click', () => {
+  darkMode = localStorage.getItem('darkMode');
+  if (darkMode !== 'enabled') {
+    enableDarkMode();  
+  } else {
+    disableDarkMode();
+  }
+});
