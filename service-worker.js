f7735e1c2dc331ffef41ce0627c8d72bf9d8a642
Maciej Twarog <85953204+maciekt07@users.noreply.github.com>
2022-08-12 20:51:27 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,21 @@
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+// workbox.routing.registerRoute(({ request }) => request.destination === "image", new workbox.strategies.NetworkFirst());
+
+const MY_CACHE = "cache-all";
+const MY_FILES = ["/css/style.css", "/js/App.js", "/js/shortcuts.js", "/img", "/img/icons", "/index.html", "/service-worker.js", "/manifest.json", "/js/ntc.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(MY_CACHE).then((cache) => {
+      return cache.addAll(MY_FILES);
+    })
+  );
+});
+// strategia 'Network falling back to cache'
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
