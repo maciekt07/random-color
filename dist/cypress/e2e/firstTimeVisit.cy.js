3eafde407c43711a6228cbba11f968bdee62db16
maciekkoks <85953204+maciekkoks@users.noreply.github.com>
2022-09-08 15:37:03 +0200

    basic changes

--- dist/cypress/e2e/firstTimeVisit.cy.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-const url = "http://127.0.0.1:5500/";
-describe("empty spec", () => {
-    it("should visit like a first time user", () => {
-        cy.clearLocalStorage();
-        cy.visit(url);
-    });
-});
